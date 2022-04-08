9ccf07535d0ceffbb4ed465f139c0513ea94ef71
lielet <berhanulielet@gmail.com>
2022-04-08 12:07:00 -0700

    task 03

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Description: print the lower and upper alphabet on the same line
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+for (c = 0; c < 26; ++c)
+putchar('A' + c);
+
+putchar('\n');
+		
+return (0);
+}
