70fd7f93e57f838ba757a2d525be36204eea25ee
lielet <berhanulielet@gmail.com>
2022-04-15 10:26:25 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - checkers upper case
+ * @c: print int
+ * Return: give 1 or 0
  */
-int main(void)
+int _isupper(int c)
 {
-	char c;
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+else 
+{
+return (0);
+}
+
 }
