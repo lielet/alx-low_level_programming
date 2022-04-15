2c1cfb345c64f81e143a3487a9e8daaf1d0e46fe
lielet <berhanulielet@gmail.com>
2022-04-15 14:51:55 -0700

    task 04

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,16 @@
  */
 void print_most_numbers(void)
 {
+
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
 	if (i == 2 || i == 4)
 	{
-		i++
+		i++;
 	}
 		_putchar(i + '0');
 	}
 	        _putchar('\n');
 }
-
