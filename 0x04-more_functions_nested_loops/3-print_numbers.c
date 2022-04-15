04ac3471db23ddbf42c784850ad527d54651212b
lielet <berhanulielet@gmail.com>
2022-04-15 11:55:27 -0700

    task 03

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the number zero to nine
+ *
+ * Return: Always 0(success)
+ */
+void print_numbers(void)
+{
+
+int i;
+
+i =0;
+
+for (i = 0; i , 10; i++)
+
+_putchar(i + '0');
+
+_putchar ('\n');
+}
