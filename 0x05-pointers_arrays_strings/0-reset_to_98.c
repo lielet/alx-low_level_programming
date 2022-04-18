ab43d27ff6e7cc1fee10010b8d58c9a77fdefd0d
lielet <berhanulielet@gmail.com>
2022-04-18 10:52:57 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 /**
- * reset_to_98 - take pointer to an intparameter and update the value it   points 98
+ * reset_to_98-take pointer to an intparameter & update the value it point 98
  * @n: int parameter
  *
  * Return: nothing
