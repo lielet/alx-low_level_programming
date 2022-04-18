14c9c71a9f69e771ab963a29fb73f7d11e25c9f1
lielet <berhanulielet@gmail.com>
2022-04-18 13:13:30 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,11 @@
 /**
- * _puts - write the character c to stdout
+ * _putchar - write the character c to stdout
  * @c: the character to print
  *
  * Return: on success 1
  * On error, -1 is returned and error is there returned
  */
-
-int _puts(char *str)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
