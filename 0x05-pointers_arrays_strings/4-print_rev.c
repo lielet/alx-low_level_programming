be8bc713e0305b619303553f5a80470aea853335
lielet <berhanulielet@gmail.com>
2022-04-18 18:17:59 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse order
+ * @s: A pointer to an int that will bw changed
+ *
+ * Return: void which means our is correct
+ */
+
+void print_rev(char *s)
+{
+int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+{
+
+for (i = i - 1 ; i >= 0; i--)
+}
+	_putchar (s[i]);
+}
+	_putchart ('\n');
+}
